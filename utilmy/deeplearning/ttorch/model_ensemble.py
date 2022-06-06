0110f5eef6baf8664b537d4d9507eb421d79cb33
Kev NoNo <arita37@users.noreply.github.com>
2022-06-06 10:48:26 +0900

    ok

--- utilmy/deeplearning/ttorch/model_ensemble.py
+++ utilmy/deeplearning/ttorch/model_ensemble.py
@@ -1585,73 +1585,8 @@ class model_create(BaseModel):
         if not loss_fun : loss_fun
         return torch.nn.BCELoss()
 
-class MultiClassMultiLabel_Head(nn.Module):
-    def __init__(self, layers_dim=[256,64],  class_label_dict=None, dropout=0,):
-        """
 
-           class_label_dict :  {'gender': 2,  'age' : 5}  ##5 n_unique_label
-
-        """
-        super().__init__()
-        self.dropout = nn.Dropout(dropout)
-        self.relu    = nn.ReLU()
-        self.class_label_dict = class_label_dict
-
-        ########Common part #################################################################
-        self.linear_list = []
-        out_dimi = layers_dim[0]
-        for i,dimi in enumerate(layers_dim[1:]) :
-            # Layer 1
-            in_dimi  = out_dimi
-            out_dimi = dimi
-            self.linear_list.append(nn.Linear(in_features=in_dimi, out_features=out_dimi, bias=False))
-        dim_final = layers_dim[-1]
-        #self.linear_list.append(nn.Linear(in_features=out_dimi, out_features=dim_final, bias=False))
-        self.linear_list = nn.Sequential(*self.linear_list)
-       
-
-        ########Multi-Class ################################################################
-        self.head_task_dict = {}
-        for classname, n_unique_label in class_label_dict.items():
-            self.head_task_dict[classname] = []
-            self.head_task_dict[classname].append(nn.Linear(dim_final, n_unique_label))
-            self.head_task_dict[classname].append(nn.Linear(n_unique_label, 1))
-            self.head_task_dict[classname] = nn.Sequential(*self.head_task_dict[classname])
-
-
-
-    def forward(self, x):
-        for lin_layer in self.linear_list:
-           x = self.relu(lin_layer(self.dropout(x)))
-        yout  = {}
-        out = 0
-        ##We are returning only tensor assuming only one class
-        for class_i in self.class_label_dict.keys():
-            out = yout[class_i] = self.head_task_dict[class_i](x)
-        return out
-
-
-    def get_loss(self,ypred, ytrue, loss_calc_custom=None,
-                 weights=None, sum_loss=True):
-        """
-
-        """
-        if loss_calc_custom is None :
-           loss_calc_fun = nn.CrossEntropyLoss()
-        else :
-           loss_calc_fun = loss_calc_custom()
-
-        loss_list = []
-        for ypred_col, ytrue_col in zip(ypred, ytrue) :
-           loss_list.append(loss_calc_fun(ypred_col, ytrue_col) )
-
-        if sum_loss:
-            weights = 1.0 / len(loss_list) * np.ones(len(loss_list))  if weights is None else weights
-            lsum = 0.0
-            for li in loss_list:
-                lsum = lsum + weights* li
-            return lsum
-        return loss_list
+from util_model import MultiClassMultiLabel_Head
 
 
 #################################################################################################
