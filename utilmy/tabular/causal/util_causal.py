edf44a710938bde5c59297b9802703fc6142aafc
arita37 <noelkev0@gmail.com>
2022-06-17 22:22:21 +0900

    ok2

--- utilmy/tabular/causal/util_causal.py
+++ utilmy/tabular/causal/util_causal.py
@@ -8,4 +8,16 @@ https://github.com/darya-chyzhyk/confound_prediction
 
 
 
+
+
+Bayesian Network Learning:
+
+
+https://erdogant.github.io/bnlearn/pages/html/UseCases.html
+
+https://causalnex.readthedocs.io/en/latest/
+
+
+
+
 """
