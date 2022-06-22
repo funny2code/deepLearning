520ed8a58ae2f4cbea0df9b80647b797bab103cb
arita37 <noelkev0@gmail.com>
2022-06-22 10:27:16 +0900

    ok

--- utilmy/deeplearning/ttorch/layers/__init__.py
+++ utilmy/deeplearning/ttorch/layers/__init__.py
@@ -5,7 +5,7 @@ from pathlib import Path
 from typing import Optional, Sequence
 
 import torch
-from torch import Tensor
+from torch import Tensor, nn
 from torch import nn
 from torch.nn import functional as F
 from torch.utils.data import DataLoader, Dataset
@@ -106,3 +106,24 @@ class MultiClassMultiLabel_Head(nn.Module):
             return lsum
         return loss_list
 
+
+class LSTM(nn.Module):
+  def __init__(self, input_size, hidden_size, num_layers, num_classes, dropout):
+    super(LSTM, self).__init__()
+    self.num_layers = num_layers
+    self.input_size = input_size
+    self.hidden_size = hidden_size
+    self.num_classes = num_classes
+    self.dropout = dropout
+
+    self.lstm = nn.LSTM(self.input_size, self.hidden_size, self.num_layers,
+                        dropout = self.dropout, batch_first=True)
+    self.fc = nn.Linear(self.hidden_size, self.num_classes)
+
+  def forward(self, x):
+    h0 = torch.zeros(self.num_layers, x.size(0), self.hidden_size)
+    c0 = torch.zeros(self.num_layers, x.size(0), self.hidden_size)
+    out, _ = self.lstm(x, (h0,c0))
+    out = out[:,-1,:]
+    out = self.fc(out)
+    return out
--- utilmy/deeplearning/ttorch/util_model.py
+++ utilmy/deeplearning/ttorch/util_model.py
@@ -12,15 +12,10 @@ import os, pickle, numpy as np
 from collections import OrderedDict
 from functools import partial
 from pathlib import Path
-from typing import Optional, Sequence
 
 import torch
-from torch import Tensor
 from torch import nn
-from torch.nn import functional as F
-from torch.utils.data import DataLoader, Dataset
 from torch.utils.data import DataLoader
-
 
 #################################################################################################
 from utilmy import log
@@ -620,131 +615,6 @@ class model_getlayer():
 
 ##################################################################################################
 ########### Custom Losses ######################################################################
-class FocalLoss(nn.Module):
-    """ Focal Loss, as described in https://arxiv.org/abs/1708.02002.
-    Docs::
-
-        It is essentially an enhancement to cross entropy loss and is
-        useful for classification tasks when there is a large class imbalance.
-        x is expected to contain raw, unnormalized scores for each class.
-        y is expected to contain class labels.
-        Shape:
-            - x: (batch_size, C) or (batch_size, C, d1, d2, ..., dK), K > 0.
-            - y: (batch_size,) or (batch_size, d1, d2, ..., dK), K > 0.
-    """
-
-    def __init__(self,
-                 alpha: Optional[Tensor] = None,
-                 gamma: float = 0.,
-                 reduction: str = 'mean',
-                 ignore_index: int = -100):
-        """Constructor.
-        Args:
-            alpha (Tensor, optional): Weights for each class. Defaults to None.
-            gamma (float, optional): A constant, as described in the paper.
-                Defaults to 0.
-            reduction (str, optional): 'mean', 'sum' or 'none'.
-                Defaults to 'mean'.
-            ignore_index (int, optional): class label to ignore.
-                Defaults to -100.
-        """
-        if reduction not in ('mean', 'sum', 'none'):
-            raise ValueError(
-                'Reduction must be one of: "mean", "sum", "none".')
-
-        super().__init__()
-        self.alpha = alpha
-        self.gamma = gamma
-        self.ignore_index = ignore_index
-        self.reduction = reduction
-
-        self.nll_loss = nn.NLLLoss(
-            weight=alpha, reduction='none', ignore_index=ignore_index)
-
-    def __repr__(self):
-        arg_keys = ['alpha', 'gamma', 'ignore_index', 'reduction']
-        arg_vals = [self.__dict__[k] for k in arg_keys]
-        arg_strs = [f'{k}={v}' for k, v in zip(arg_keys, arg_vals)]
-        arg_str = ', '.join(arg_strs)
-        return f'{type(self).__name__}({arg_str})'
-
-    def forward(self, x: Tensor, y: Tensor) -> Tensor:
-        if x.ndim > 2:
-            # (N, C, d1, d2, ..., dK) --> (N * d1 * ... * dK, C)
-            c = x.shape[1]
-            x = x.permute(0, *range(2, x.ndim), 1).reshape(-1, c)
-            # (N, d1, d2, ..., dK) --> (N * d1 * ... * dK,)
-            y = y.view(-1)
-
-        unignored_mask = y != self.ignore_index
-        y = y[unignored_mask]
-        if len(y) == 0:
-            return 0.
-        x = x[unignored_mask]
-
-        # compute weighted cross entropy term: -alpha * log(pt)
-        # (alpha is already part of self.nll_loss)
-        log_p = F.log_softmax(x, dim=-1)
-        ce = self.nll_loss(log_p, y)
-
-        # get true class column from each row
-        all_rows = torch.arange(len(x))
-        log_pt = log_p[all_rows, y]
-
-        # compute focal term: (1 - pt)^gamma
-        pt = log_pt.exp()
-        focal_term = (1 - pt)**self.gamma
-
-        # the full loss: -alpha * ((1 - pt)^gamma) * log(pt)
-        loss = focal_term * ce
-
-        if self.reduction == 'mean':
-            loss = loss.mean()
-        elif self.reduction == 'sum':
-            loss = loss.sum()
-
-        return loss
-
-
-def focal_loss(alpha: Optional[Sequence] = None,
-               gamma: float = 0.,
-               reduction: str = 'mean',
-               ignore_index: int = -100,
-               device='cpu',
-               dtype=torch.float32) -> FocalLoss:
-    """Factory function for FocalLoss.
-    Docs::
-
-            alpha (Sequence, optional): Weights for each class. Will be converted
-                to a Tensor if not None. Defaults to None.
-            gamma (float, optional): A constant, as described in the paper.
-                Defaults to 0.
-            reduction (str, optional): 'mean', 'sum' or 'none'.
-                Defaults to 'mean'.
-            ignore_index (int, optional): class label to ignore.
-                Defaults to -100.
-            device (str, optional): Device to move alpha to. Defaults to 'cpu'.
-            dtype (torch.dtype, optional): dtype to cast alpha to.
-                Defaults to torch.float32.
-        Returns:
-            A FocalLoss object
-    """
-    if alpha is not None:
-        if not isinstance(alpha, Tensor):
-            alpha = torch.tensor(alpha)
-        alpha = alpha.to(device=device, dtype=dtype)
-
-    fl = FocalLoss(
-        alpha=alpha,
-        gamma=gamma,
-        reduction=reduction,
-        ignore_index=ignore_index)
-    return
-
-
-
-
-
 
 
 ##################################################################################################
@@ -938,29 +808,6 @@ class MultiClassMultiLabel_Head(nn.Module):
                 lsum = lsum + wi * li
             return lsum
         return loss_list
-
-
-class LSTM(nn.Module):
-  def __init__(self, input_size, hidden_size, num_layers, num_classes, dropout):
-    super(LSTM, self).__init__()
-    self.num_layers = num_layers
-    self.input_size = input_size
-    self.hidden_size = hidden_size
-    self.num_classes = num_classes
-    self.dropout = dropout
-
-    self.lstm = nn.LSTM(self.input_size, self.hidden_size, self.num_layers,
-                        dropout = self.dropout, batch_first=True)
-    self.fc = nn.Linear(self.hidden_size, self.num_classes)
-
-  def forward(self, x):
-    h0 = torch.zeros(self.num_layers, x.size(0), self.hidden_size)
-    c0 = torch.zeros(self.num_layers, x.size(0), self.hidden_size)
-    out, _ = self.lstm(x, (h0,c0))
-    out = out[:,-1,:]
-    out = self.fc(out)
-    return out
-
 
 
 class SequenceReshaper(nn.Module):
