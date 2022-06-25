a84e10ce35772e3bbdac4aeecaba6cbe215b13b6
fangfang.shang <fangfang.shang@amh-group.com>
2022-06-25 14:31:17 +0800

    fix

--- utilmy/sspark/src/util_hadoop.py
+++ utilmy/sspark/src/util_hadoop.py
@@ -248,73 +248,73 @@ hdfs_dir_stats = hdfs_dir_info
 
 
 def hdfs_download(dirin="", dirout="./", verbose=False, n_pool=1, **kw):
-  """  Donwload files in parallel using pyarrow
-  Doc::
+    """  Donwload files in parallel using pyarrow
+    Doc::
 
         path_glob: list of HDFS pattern, or sep by ";"
         :return:
-  """
-  import glob, gc,os
-  from multiprocessing.pool import ThreadPool
+    """
+    import glob, gc,os
+    from multiprocessing.pool import ThreadPool
 
-  def log(*s, **kw):
-      log(*s, flush=True, **kw)
+    def log(*s, **kw):
+        log(*s, flush=True, **kw)
 
-  #### File ############################################
-  import pyarrow as pa
-  hdfs  = pa.hdfs.connect()
-  flist = [t for t in hdfs.ls(dirin)]
+    #### File ############################################
+    import pyarrow as pa
+    hdfs  = pa.hdfs.connect()
+    flist = [t for t in hdfs.ls(dirin)]
 
-  def fun_async(x):
-      hdfs.download(x[0], x[1])
-
-
-  ######################################################
-  file_list = sorted(list(set(flist)))
-  n_file    = len(file_list)
-  if verbose: log(file_list)
-
-  #### Pool count
-  if   n_pool < 1  :  n_pool = 1
-  if   n_file <= 0 :  m_job  = 0
-  elif n_file <= 2 :
-    m_job  = n_file
-    n_pool = 1
-  else  :
-    m_job  = 1 + n_file // n_pool  if n_file >= 3 else 1
-  if verbose : log(n_file,  n_file // n_pool )
-
-  pool   = ThreadPool(processes=n_pool)
-
-  res_list=[]
-  for j in range(0, m_job ) :
-      if verbose : log("Pool", j, end=",")
-      job_list = []
-      for i in range(n_pool):
-         if n_pool*j + i >= n_file  : break
-         filei = file_list[n_pool*j + i]
-
-         xi    = (filei, dirout + "/" + filei.split("/")[-1])
-
-         job_list.append( pool.apply_async(fun_async, (xi, )))
-         if verbose : log(j, filei)
-
-      for i in range(n_pool):
-        if i >= len(job_list): break
-        res_list.append( job_list[ i].get() )
+    def fun_async(x):
+        hdfs.download(x[0], x[1])
 
 
-  pool.close()
-  pool.join()
-  pool = None
-  if m_job>0 and verbose : log(n_file, j * n_file//n_pool )
-  return res_list
+    ######################################################
+    file_list = sorted(list(set(flist)))
+    n_file    = len(file_list)
+    if verbose: log(file_list)
+
+    #### Pool count
+    if   n_pool < 1  :  n_pool = 1
+    if   n_file <= 0 :  m_job  = 0
+    elif n_file <= 2 :
+        m_job  = n_file
+        n_pool = 1
+    else  :
+        m_job  = 1 + n_file // n_pool  if n_file >= 3 else 1
+    if verbose : log(n_file,  n_file // n_pool )
+
+    pool   = ThreadPool(processes=n_pool)
+
+    res_list=[]
+    for j in range(0, m_job ) :
+        if verbose : log("Pool", j, end=",")
+        job_list = []
+        for i in range(n_pool):
+            if n_pool*j + i >= n_file  : break
+            filei = file_list[n_pool*j + i]
+
+            xi    = (filei, dirout + "/" + filei.split("/")[-1])
+
+            job_list.append( pool.apply_async(fun_async, (xi, )))
+            if verbose : log(j, filei)
+
+        for i in range(n_pool):
+            if i >= len(job_list): break
+            res_list.append( job_list[ i].get() )
+
+
+    pool.close()
+    pool.join()
+    pool = None
+    if m_job>0 and verbose : log(n_file, j * n_file//n_pool )
+    return res_list
 
  
 
 
 
- def hive_get_tablelist(dbname):
+def hive_get_tablelist(dbname):
     """Get Hive tables from database_name
     """
     cmd = f"hive -e 'show tables from {dbname}'"
