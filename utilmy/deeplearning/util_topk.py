b579ba6119804305d9cd4321c6c00e2ac592e09f
arita37 <noelkev0@gmail.com>
2022-07-04 09:33:38 +0900

    ok

--- utilmy/deeplearning/util_topk.py
+++ utilmy/deeplearning/util_topk.py
@@ -69,30 +69,21 @@ def test_all() -> None:
 def test2():
   """  tests
   Docs ::
+  
         Test Cases  pd_add_onehot_encoding
-            1. check if id column is present in dfref
-            2. check if labels_col are present in dfref 
 
         Test Cases  embedding_cosinus_scores_pairwise
-            1. check if length of word_list is same as length of emb
+
 
         Test Cases  KNNClassifierFAISS
-            1. In fit function check if input and output dimensions are same
-            2. If algorithm is vornoni, check if number of input vecs >= number of clusters
+
 
         Test Cases  faiss_create_index
-            1. check if df_or_path is not a string instance mentioning dirout is mandatory
-            2. check if id column in present in input dataframe
-            3. check if mentioned col parameter value is present in input dataframe
-            4. check if dimension of dataframe embs is same as emb_dim
-            5. Check if dimension D is a multiple of number of sub_quantizers
-            6. Check if number of training inputs >= number of clusters   
+ 
 
         Test Cases  topk_calc
-            1. check if dimension of dataframe embs is same as emb_dim
 
-         Test Cases  faiss_topk_calc
-            1. check if colid and colemb columns are present in input dataframe
+        Test Cases  faiss_topk_calc
 
   """
   dd = test_create_fake_df(dirout= "./")
@@ -101,35 +92,38 @@ def test2():
   for i in range(4):
       emb_list.append( ','.join([str(x) for x in np.random.random(200)]))
 
-  res = pd.DataFrame({'id': [1,2,3,4] * 6000, 
+  df = pd.DataFrame({'id': [1,2,3,4] * 6000, 
                       'gender': [0,1,0,1] * 6000, 
                       'masterCategory': [2,1,3,4] * 6000, 
                       'emb': emb_list * 6000})
 
-  labels_dict = {
-      'gender' : [0,1],
-      'masterCategory' : [3,1,2],
+  labels_dict = { 'gender' : [0,1],
+                  'masterCategory' : [3,1,2],
   }
 
   path = './temp/tem/'
   os.makedirs(path, exist_ok=True)
-  res.to_csv(f'{path}1.csv', index=False)
+  df.to_csv(f'{path}1.csv', index=False)
 
   log('######### pd_to_onehot #####################')
-  df = pd_add_onehot_encoding(dfref=res, img_dir=f'{path}/1.csv', labels_col=labels_dict )
+  df = pd_to_onehot(df,  labels_dict=labels_dict )
 
   log('######### embedding cosinus #####################')
   embedding_cosinus_scores_pairwise(embs=np.random.random((5,5)), word_list=np.array([1,2,3,4,5]))
 
+
   log("#########  faiss_KNNClassifier ###################################")
-  k = KNNClassifierFAISS()
+  k = faiss_KNNClassifier()
   k = k.fit(np.random.random((5,5)), np.array([0,1,2,3,4]))
+
 
   log("#########  faiss_create_index  ##################################")
   faiss_create_index(df_or_path=f'{path}1.csv')
 
+
   log("#########  topk_calc  ##################################")
   topk_calc(diremb=f'{path}1.csv', nexample=24000)
+
   
   log("#########  faiss_topk_calc  ##################################")
   faiss_topk_calc(df=f'{path}1.csv', root=path, colid='id', colemb='emb',
