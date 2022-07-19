06c2fcdaa6636873ec68c164e6ba9c54d7f35548
arita37 <noelkev0@gmail.com>
2022-07-19 12:00:32 +0900

    ok

--- utilmy/oos.py
+++ utilmy/oos.py
@@ -1141,21 +1141,21 @@ def glob_glob(dirin, exclude="", include_only="",
             ndays_past=3000, nmin_past=0,  start_date='1970-01-01', end_date='2050-01-01',
             nfiles=99999999, verbose=0,
 ):
-    """ Advanced Glob filtering
+    """ Advanced Glob filtering.
     Docs:
 
-        https://www.twilio.com/blog/working-with-files-asynchronously-in-python-using-aiofiles-and-asyncio
+            https://www.twilio.com/blog/working-with-files-asynchronously-in-python-using-aiofiles-and-asyncio
 
-        dirin
-        exclude=""
-        include_only=""
-        min_size_mb=0
-        max_size_mb=500000
-        ndays_past=3000
-        start_date='1970-01-01'
-        end_date='2050-01-01'
-        nfiles=99999999
-        verbose=0
+            dirin
+            exclude=""
+            include_only=""
+            min_size_mb=0
+            max_size_mb=500000
+            ndays_past=3000
+            start_date='1970-01-01'
+            end_date='2050-01-01'
+            nfiles=99999999
+            verbose=0
 
     """
     import glob, copy, datetime as dt, time
