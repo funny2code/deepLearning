b2890468aa1c7f63fbfecec9c42c0247084ef774
Kev NoNo <arita37@users.noreply.github.com>
2022-08-23 23:56:14 +0900

    ok

--- utilmy/cloud/aws/util_aws.py
+++ utilmy/cloud/aws/util_aws.py
@@ -1,5 +1,10 @@
 # -*- coding: utf-8 -*-
-"""
+""" Utils for AWS
+Docs::
+
+    https://loige.co/aws-command-line-s3-content-from-stdin-or-to-stdout/
+
+    
 
 
 
@@ -111,22 +116,23 @@ def glob_s3(path: str = None, recursive: bool = True, max_items_per_api_call: st
 
     # Create cmd to list all objects with default pagination
     cmd = ["aws", "s3api", "list-objects-v2", "--bucket", bucket_name, "--output", "json",
-           "--query", "{Contents: Contents[]." + sfield + "  ,NextToken: "
+            "--query", "{Contents: Contents[]." + sfield + "  ,NextToken: "
                           "NextToken}"]
 
     # If pagination is not required, add flag
-    if not recursive:  cmd.append("--no-paginate")
+    if not recursive:
+        cmd.append("--no-paginate")
     else:
         # Note : max_items_per_api_call * 1000 is the limit of files that this function can process
         cmd.extend(["--max-items", str(max_items_per_api_call)])
 
     # If only specific path is needed to be listed, add it
-    if path:          cmd.extend(["--prefix", path])
+    if path:   cmd.extend(["--prefix", path])
 
     # If any extra params were passed, add them here
-    if extra_params:  cmd.extend(extra_params)
+    if extra_params:   cmd.extend(extra_params)
 
-
+    # run cmd and return files data
     files_data = s3_get_filelist_cmd(cmd)
 
     if 'tuple' in return_format:
@@ -137,10 +143,85 @@ def glob_s3(path: str = None, recursive: bool = True, max_items_per_api_call: st
         if xi.get("LastModified", None):  xlist += (xi['LastModified'],)
         if xi.get("Output", None):        xlist += (xi['Output'],)
         flist.append(xlist)
-
-      return flist
+      print(flist)
     else :  
       return files_data
+
+
+
+def s3_load_file(s3_path: str = None, 
+                 extra_params: list = None, 
+                 return_stream: bool = False, 
+                 is_binary: bool = False) -> Union[str, IO, bytes]:
+    """ Load file in memory using AWS CLI
+    Docs::
+
+          file_data = get_data(s3_path="", extra_params=[])
+          
+          extra params:
+          return_stream:  return as stream data
+          is_binary :     return as binary string
+
+
+          Infos:
+             cmd = ["aws", "s3", "cp", s3_path, "-"]
+
+             https://loige.co/aws-command-line-s3-content-from-stdin-or-to-stdout/
+
+             https://aws.amazon.com/blogs/media/processing-user-generated-content-using-aws-lambda-and-ffmpeg/
+
+             https://stackoverflow.com/questions/48725405/how-to-read-binary-data-over-a-pipe-from-another-process-in-python
+
+
+    """             
+    from subprocess import PIPE, Popen
+
+    cmd = ["aws", "s3", "cp", s3_path, "-"]
+
+    # If any extra params were passed, add them here
+    if extra_params:   cmd.extend(extra_params)
+
+    # Run cmd
+    proc = Popen(cmd, stdout=PIPE, stderr=PIPE)
+
+    # If we need a stream
+    if return_stream: return proc.stdout
+
+
+    # If we need to return the data only
+    file_data = ""
+    if not is_binary:
+        for this_line in iter(proc.stdout.readline, b''):
+
+            # Poll for return code
+            proc.poll()
+            # If return code exists exit from loop
+            if proc.returncode is not None:
+                break
+
+            # Decode the binary stream
+            this_line_decoded = this_line.decode("utf8")
+            if this_line_decoded:
+                # In case you want to have stdout as well
+                # If removed there will be no indication that we are still receiving the data
+                print(this_line_decoded)
+                file_data = file_data + "\n" + this_line_decoded
+    else:
+        for this_bit in iter(proc.stdout.read, b''):
+            file_data = bytes()
+            print(this_bit, sep="", end="")
+            file_data = file_data + this_bit
+
+    # If the process returncode is None and we reach here, start polling for returncode until it exists
+    while proc.returncode is None:
+        proc.poll()
+
+    # raise exception if error occurred, else return file_data
+    if proc.returncode != 0 and proc.returncode is not None:
+        _, err = proc.communicate()
+        raise Exception(f"Error occurred with exit code {proc.returncode}\n{str(err.decode('utf8'))}")
+    elif proc.returncode == 0:
+        return file_data
 
 
 
