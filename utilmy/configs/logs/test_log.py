95184e1fdd1c453cb3d1c55b3b4aef50a4c7474c
arita37 <noelkev0@gmail.com>
2022-09-13 00:51:20 +0900

    ok

--- utilmy/configs/logs/test_log.py
+++ utilmy/configs/logs/test_log.py
@@ -6,13 +6,20 @@ python test.py  test2
 
 
 """
+import sys, os
+
+
+##############################################################################
+def test_all():
+
+    test1()
+    test2()
+    test_logging()
+
+
+
 def test1():
     """function test1.
-    Doc::
-            
-            Args:
-            Returns:
-                
     """
     from util_log import log3, log2, log, logw, loge, logc, logr
     log3("debug2")
@@ -29,16 +36,11 @@ def test1():
 
     log("finish")
 
-
+        
 
 
 def test2():
     """function test2.
-    Doc::
-            
-            Args:
-            Returns:
-                
     """
 
     print("\n\n\n########## Test 2############################")
@@ -66,6 +68,70 @@ def test2():
 
 
 
+def test_logging():
+    import logging
+    import os
+    os.environ['log_verbosity']='10'
+    os.environ['log_type']='logging'
+    from util_log import logger, log3, log2, log, logw, loge, logc, logr, logger_setup
+    from util_log import FORMATTER_1,FORMATTER_2,FORMATTER_3,FORMATTER_4,FORMATTER_5
+    
+   
+
+    s='test logger_name'
+    print('\ntest logger_name')
+    logger=logger_setup(logger_name= 'test_logging' )
+    log2(s)
+
+    # test log_fomatter
+    print('\ntest log_fomatter ')
+    log_formats=[FORMATTER_1, FORMATTER_2,FORMATTER_3,FORMATTER_4,FORMATTER_5]
+    for format in log_formats:
+        logger=logger_setup(formatter=format)
+        log2(s)
+
+    # test isrotate
+    s='test isrotate'
+    print('\ntest isrotate ')
+    logger=logger_setup(isrotate=True)
+    log2(s)
+
+    # test isconsole_output
+    s='test isconsole_output'
+    print('\ntest isconsole_output ')
+    logger=logger_setup(isconsole_output=False)
+    log2(s)
+
+    # test logging_level
+    print('\ntest logging levels')
+    log_levels=[logging.CRITICAL, logging.ERROR,logging.WARNING,logging.INFO,logging.DEBUG]
+    
+    for log in log_levels:
+        logger=logger_setup(logging_level=log)
+        if log==logging.CRITICAL:
+            s='critical'
+            logc(s)
+        elif log==logging.ERROR:
+            s='error'
+            loge(s)
+        elif log==logging.WARNING:
+            s='warning'
+            logw(s)
+        elif log==logging.INFO:
+            s='info'
+            log2(s)
+        elif log==logging.DEBUG:
+            s='debug'
+            log3(s)
+
+
+    # test log_file
+    s='test log_file'
+    print('\ntest log_file test.log')
+    logger=logger_setup(log_file='test.log')
+    log2(s)
+  
+
 
 ###################################################################################
 ###################################################################################
@@ -74,7 +140,7 @@ import pickle
 import struct
 import json
 
-from loguru import logger
+# from loguru import logger
 
 class LoggingStreamHandler(socketserver.StreamRequestHandler):
     def handle(self):
@@ -125,6 +191,7 @@ def test_server():
     """
 
     print("\n\n\n########## Test 2############################")
+    os.environ['log_type'] = 'loguru'
     import util_log
 
 
@@ -152,8 +219,9 @@ def test_server():
 
 ############################################################################
 if __name__ == "__main__":
-	import fire
-	fire.Fire()
+    test_logging()
+	# import fire
+	# fire.Fire()
 
 
 """
