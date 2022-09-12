e724c15431f67c46bf2ea7b03cfaf730ed294d79
arita37 <noelkev0@gmail.com>
2022-09-13 00:55:02 +0900

    ok2

--- utilmy/configs/logs/test_log.py
+++ utilmy/configs/logs/test_log.py
@@ -6,7 +6,7 @@ python test.py  test2
 
 
 """
-import sys, os
+import sys, os, logging
 
 
 ##############################################################################
@@ -69,8 +69,6 @@ def test2():
 
 
 def test_logging():
-    import logging
-    import os
     os.environ['log_verbosity']='10'
     os.environ['log_type']='logging'
     from util_log import logger, log3, log2, log, logw, loge, logc, logr, logger_setup
@@ -105,7 +103,7 @@ def test_logging():
     # test logging_level
     print('\ntest logging levels')
     log_levels=[logging.CRITICAL, logging.ERROR,logging.WARNING,logging.INFO,logging.DEBUG]
-    
+
     for log in log_levels:
         logger=logger_setup(logging_level=log)
         if log==logging.CRITICAL:
@@ -133,95 +131,11 @@ def test_logging():
   
 
 
-###################################################################################
-###################################################################################
-import socketserver
-import pickle
-import struct
-import json
-
-# from loguru import logger
-
-class LoggingStreamHandler(socketserver.StreamRequestHandler):
-    def handle(self):
-        """ LoggingStreamHandler:handle.
-        Doc::
-                
-                    Args:
-                    Returns:
-                       
-        """
-        while True:
-            chunk = self.connection.recv(4)
-            if len(chunk) < 4:
-                break
-            slen = struct.unpack('>L', chunk)[0]
-            chunk = self.connection.recv(slen)
-            while len(chunk) < slen:
-                chunk = chunk + self.connection.recv(slen - len(chunk))
-            record = pickle.loads(chunk)
-            #print(json.loads(record['msg']))
-            level, message = record["levelname"], json.loads(record["msg"])['text']
-            logger.patch(lambda record: record.update(record)).log(level, message)
-
-
-def test_launch_server():
-	'''.
-	Doc::
-	        
-	    	Server code from loguru.readthedocs.io
-	    	Use to test network logging
-	    
-	         python   test.py test_launch_server
-	    
-	    
-	'''
-	PORT = 5000 #Make sure to set the same port defined in logging template	
-	server = socketserver.TCPServer(('localhost', PORT), LoggingStreamHandler)
-	server.serve_forever()
-
-
-def test_server():
-    """function test_server.
-    Doc::
-            
-            Args:
-            Returns:
-                
-    """
-
-    print("\n\n\n########## Test 2############################")
-    os.environ['log_type'] = 'loguru'
-    import util_log
-
-
-    from util_log import log3, log2, log, logw, loge, logc, logr
-
-    ### Redefine new template
-    util_log.logger_setup('config_loguru.yaml', 'server_socket')
-
-    log3("debug2")
-    log2("debug")
-    log("info")
-    logw("warning")
-    logc("critical")
-
-    try:
-        a = 1 / 0
-    except Exception as e:
-        logr("error", e)
-        loge("Exception"), e
-
-    log("finish")
-
-
-
-
 ############################################################################
 if __name__ == "__main__":
-    test_logging()
-	# import fire
-	# fire.Fire()
+    # test_logging()
+    import fire
+    fire.Fire()
 
 
 """
