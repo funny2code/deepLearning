2b3a14e06ba980908cade6bfae82069401998f0c
arita37 <noelkev0@gmail.com>
2022-09-14 00:24:23 +0900

    ok

--- utilmy/utilmy_base.py
+++ utilmy/utilmy_base.py
@@ -938,8 +938,13 @@ def test_all():
     test4()
     test5()
     test7()
-    test_save_and_load()
-    test_to_file()
+    test1()
+    test2()
+    test3()
+    test4()
+    test5()
+    test6()
+    test7()
 
 
 def test1():
@@ -1099,19 +1104,17 @@ def test7():
     data_for_save = "data_for_save"
     m.save(data_for_save, "./testfile")
     loaded_data = m.load("./testfile")
-
-
-    os.remove("./testfile")
     assert loaded_data == data_for_save, "FAILED -> save and load"
+
 
     log("\n####", m.to_file)
     to_file("some_text_data","./testfile",mode="w")
     with open("./testfile", mode="r") as fp:
         file_content = fp.read()
-    os.remove("./testfile")
     assert file_content == "some_text_data", "FAILED -> to_file"
 
 
+    os.remove("./testfile")
 
 
 
