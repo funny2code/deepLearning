7cf620d94e49d4c39a315b8c84185331cfcca380
arita37 <noelkev0@gmail.com>
2022-09-14 21:43:39 +0900

    ok

--- utilmy/oos.py
+++ utilmy/oos.py
@@ -273,8 +273,8 @@ def test6_os():
 
 
     log("#######   os_path_size() ..")
-    size_ = os_path_size()
-    log("total size", size_)
+    size_ = os_path_size(drepo)
+    assert log("total size", size_) and size_> 10 , f"error {size_}"
 
 
     log("#######   os_path_split() ..")
