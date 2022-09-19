e26f0f969d793987cda13bee7edc4e9ff6ba8f84
arita37 <noelkev0@gmail.com>
2022-09-19 13:34:20 +0900

    ok

--- utilmy/oos.py
+++ utilmy/oos.py
@@ -1393,6 +1393,9 @@ def os_cpu_info():
     cmd = """ awk '{u=$2+$4; t=$2+$4+$5; if (NR==1){u1=u; t1=t;} else print ($2+$4-u1) * 100 / (t-t1) "%"; }' <(grep 'cpu ' /proc/stat) <(sleep 1;grep 'cpu ' /proc/stat) """
     cpu_usage = os_system(cmd)
 
+    ddict= {'ncpu': ncpu, 'cpu_usage': cpu_usage}
+    return ddict
+
 
 def os_ram_info():
     """ Get total memory and memory usage in linux
