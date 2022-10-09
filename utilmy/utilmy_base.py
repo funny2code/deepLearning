ead00b77e685b0c656106731c6cee737ddcef23f
arita37 <noelkev0@gmail.com>
2022-10-09 17:49:50 +0900

    ok

--- utilmy/utilmy_base.py
+++ utilmy/utilmy_base.py
@@ -549,7 +549,7 @@ def load_function_uri(uri_name: str="MyFolder/myfile.py:my_function"):
 
 
 ### Generic Date function   #####################################################
-def date_now(datenow:Union[str,int,datetime.datetime]="", fmt="%Y%m%d",
+def date_now(datenow:Union[str,int,float,datetime.datetime]="", fmt="%Y%m%d",
              add_days=0,  add_mins=0, add_hours=0, add_months=0,add_weeks=0,
              timezone='Asia/Tokyo', fmt_input="%Y-%m-%d",
              force_dayofmonth=-1,   ###  01 first of month
@@ -576,18 +576,20 @@ def date_now(datenow:Union[str,int,datetime.datetime]="", fmt="%Y%m%d",
     from pytz import timezone as tzone
     import datetime, time
 
-
     sdt = str(datenow)
 
     if isinstance(datenow, datetime.datetime):
         now_utc = datenow
 
-    elif isinstance(datenow, int)  and sdt[:2] == '16':  ### Unix time stamp
-        now_utc = datetime.datetime.utcfromtimestamp(datenow)
+    elif (isinstance(datenow, float) or isinstance(datenow, int)   )  and  datenow > 1600100100  :  ### Unix time stamp
+        ## unix seconds in UTC 
+        # fromtimestamp give you the date and time in local time
+        # utcfromtimestamp gives you the date and time in UTC.
+        #  int(time.time()) - date_now( int(time.time()), returnval='unix', timezone='utc') == 0 
+        now_utc = datetime.datetime.fromtimestamp(datenow)   ## 
 
-    elif  len(sdt) >7 :  ## Not None
+    elif  len(sdt) >7 :  ## date in string
         now_utc = datetime.datetime.strptime(sdt, fmt_input)
-
 
     else:
         now_utc = datetime.datetime.now(tzone('UTC'))  # Current time in UTC
@@ -605,7 +607,9 @@ def date_now(datenow:Union[str,int,datetime.datetime]="", fmt="%Y%m%d",
         now_utc = now_utc.replace(hour=force_hourofday)
 
 
-    now_new = now_utc.astimezone(tzone(timezone))  if timezone != 'utc' else  now_utc.astimezone(tzone('UTC'))
+    # now_new = now_utc.astimezone(tzone(timezone))  if timezone != 'utc' else  now_utc.astimezone(tzone('UTC'))
+    now_new = now_utc.astimezone(tzone('UTC'))  if timezone in {'utc', 'UTC'} else now_utc.astimezone(tzone(timezone)) 
+
     now_new = now_new + datetime.timedelta(days=add_days + 7*add_weeks, hours=add_hours, minutes=add_mins,)
 
 
@@ -615,8 +619,10 @@ def date_now(datenow:Union[str,int,datetime.datetime]="", fmt="%Y%m%d",
 
     if   returnval == 'datetime': return now_new ### datetime
     elif returnval == 'int':      return int(now_new.strftime(fmt))
-    elif returnval == 'unix':     return time.mktime(now_new.timetuple())
+    elif returnval == 'unix':     return datetime.datetime.timestamp(now_new)  #time.mktime(now_new.timetuple())
     else:                         return now_new.strftime(fmt)
+
+
 
 
 
@@ -1072,6 +1078,44 @@ def test_all():
     """function test_all
     """
     test1()
+    test_datenow()
+
+
+
+def test_datenow():
+    import utilmy as m, time
+
+    log("\n####", m.date_now)
+    assert m.date_now(timezone='Asia/Tokyo')    #-->  "20200519"   ## Today date in YYYMMDD
+    assert m.date_now(timezone='Asia/Tokyo', fmt='%Y-%m-%d')    #-->  "2020-05-19"
+
+    x = m.date_now('2020-12-10', fmt='%Y%m%d', add_days=-5, returnval='int')
+    assert not log( x ) and x == 20201205, x   #-->  20201205
+
+    x = m.date_now(20211005,     fmt_input='%Y%m%d', returnval='unix')
+    assert   not log(x ) and  int(x)  > 1603424400, x  #-->  1634324632848
+
+    x = m.date_now(20211005,     fmt='%Y-%m-%d', fmt_input='%Y%m%d', returnval='str')  #-->  '2021-10-05'
+    assert   not log(x ) and  x  == '2021-10-05' , x                                   #-->  1634324632848
+
+    
+    assert date_now('2020-05-09', add_months=-2, fmt='%Y-%m-%d') == "2020-03-09" #Test adding -2 months
+    assert date_now('2012-12-06 12:00:00',returnval='datetime',add_mins=20,fmt_input="%Y-%m-%d %H:%M:%S") == date_now('2012-12-06 12:20:00',returnval='datetime',fmt_input="%Y-%m-%d %H:%M:%S") #Test adding 20 minutes
+    assert date_now('2012-12-06 12:00:00',returnval='datetime',add_hours=11,fmt_input="%Y-%m-%d %H:%M:%S") == date_now('2012-12-06 23:00:00',returnval='datetime',fmt_input="%Y-%m-%d %H:%M:%S") #Test adding 11 hours
+    assert date_now('2012-12-06 12:00:00',returnval='datetime',add_days=5,fmt_input="%Y-%m-%d %H:%M:%S") == date_now('2012-12-11 12:00:00',returnval='datetime',fmt_input="%Y-%m-%d %H:%M:%S") #Test adding 5 days
+    # assert date_now('2012-12-06 19:00:00',returnval='datetime',force_dayofweek=0,fmt_input="%Y-%m-%d %H:%M:%S") == date_now('2012-12-03 19:00:00',returnval='datetime',fmt_input="%Y-%m-%d %H:%M:%S") #Test forcing day 3 of the week
+
+    x = date_now( time.time(), returnval='datetime')
+    x = date_now( time.time(), returnval='datetime', timezone='utc')
+    x = date_now( int(time.time()), returnval='datetime')
+
+    log("check unix epoch timezone time.time() ")
+    assert abs(int(time.time()) - date_now( int(time.time()), returnval='unix', timezone='utc')) < 1e-2, ""
+
+    ts  = time.time()
+    datetime.datetime.fromtimestamp(ts).strftime('%Y-%m-%d %H:%M:%S')
+    dtu = datetime.datetime.fromtimestamp(ts) #.strftime('%Y-%m-%d %H:%M:%S')
+    datetime.datetime.timestamp(dtu )  
 
 
 
@@ -1080,7 +1124,6 @@ def test1():
     drepo, dtmp = dir_testinfo()
 
     ###################################################################
-
     log("\n##### git_repo_root  ", m.git_repo_root())
     assert not m.git_repo_root() == None, "err git repo"
 
@@ -1089,7 +1132,6 @@ def test1():
 
 
     ####################################################################
-
     log("\n##### global_verbosity  ")
     log('verbosity', m.global_verbosity(__file__, "config.json", 40,))
     log('verbosity', m.global_verbosity('../', "config.json", 40,))
@@ -1101,8 +1143,8 @@ def test1():
     gverbosity =m.global_verbosity(__file__, "config.json", 40,)
     assert gverbosity == verbosity, "incorrect verbosity "
 
-    ####################################################################
 
+    ####################################################################
     file_name = f"{dtmp}/test_file_{int(time.time())}.txt"
     index = m.Index0(file_name, min_chars=8)
     data  = [ "testestest", 'duplicate', '5char', '8charlong', '### comment line, so skipped',]
@@ -1185,25 +1227,6 @@ def test1():
 
     ####################################################################
 
-    log("\n####", m.date_now)
-    assert m.date_now(timezone='Asia/Tokyo')    #-->  "20200519"   ## Today date in YYYMMDD
-    assert m.date_now(timezone='Asia/Tokyo', fmt='%Y-%m-%d')    #-->  "2020-05-19"
-
-    x = m.date_now('2020-12-10', fmt='%Y%m%d', add_days=-5, returnval='int')
-    assert not log( x ) and x == 20201205, x   #-->  20201205
-
-    x = m.date_now(20211005,     fmt_input='%Y%m%d', returnval='unix')
-    assert   not log(x ) and  int(x)  > 1603424400, x  #-->  1634324632848
-
-    x = m.date_now(20211005,     fmt='%Y-%m-%d', fmt_input='%Y%m%d', returnval='str')  #-->  '2021-10-05'
-    assert   not log(x ) and  x  == '2021-10-05' , x                                   #-->  1634324632848
-
-    
-    assert date_now('2020-05-09', add_months=-2, fmt='%Y-%m-%d') == "2020-03-09" #Test adding -2 months
-    assert date_now('2012-12-06 12:00:00',returnval='datetime',add_mins=20,fmt_input="%Y-%m-%d %H:%M:%S") == date_now('2012-12-06 12:20:00',returnval='datetime',fmt_input="%Y-%m-%d %H:%M:%S") #Test adding 20 minutes
-    assert date_now('2012-12-06 12:00:00',returnval='datetime',add_hours=11,fmt_input="%Y-%m-%d %H:%M:%S") == date_now('2012-12-06 23:00:00',returnval='datetime',fmt_input="%Y-%m-%d %H:%M:%S") #Test adding 11 hours
-    assert date_now('2012-12-06 12:00:00',returnval='datetime',add_days=5,fmt_input="%Y-%m-%d %H:%M:%S") == date_now('2012-12-11 12:00:00',returnval='datetime',fmt_input="%Y-%m-%d %H:%M:%S") #Test adding 5 days
-    # assert date_now('2012-12-06 19:00:00',returnval='datetime',force_dayofweek=0,fmt_input="%Y-%m-%d %H:%M:%S") == date_now('2012-12-03 19:00:00',returnval='datetime',fmt_input="%Y-%m-%d %H:%M:%S") #Test forcing day 3 of the week
 
 
 
@@ -1239,7 +1262,6 @@ def test1():
 
 
     ####################################################################
-
     score_word_dict = {}
     word = 'Catherine M Gitau'
     wlist = ['Catherine Gitau', 'Gitau Catherine']
