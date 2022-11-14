94874eb8e348f8a07ca352ff97f6fcfa46d5b805
arita37 <noelkev0@gmail.com>
2022-11-14 21:29:30 +0900

    ok

--- utilmy/viz/ddash/app1/app.py
+++ utilmy/viz/ddash/app1/app.py
@@ -90,6 +90,7 @@ except : pass
 
 
 
+
 #####################################################################
 def test1(homepage="main_page.py"):
     """  python  app.py test1
@@ -160,9 +161,16 @@ def export(name="app1", dirout=""):
     shutil.copytree( dir_repo + "/viz/ddash/app1/", dirout, dirs_exist_ok=True )
 
 
+
+
 ###################################################################
-######  Utils #####################################################
+######  Callback  #####################################################
 ### Main page resource
+### Callback Global for Javascript part
+"""
+
+
+"""
 app.clientside_callback(
     ClientsideFunction(
         namespace='clientside',
@@ -174,16 +182,25 @@ app.clientside_callback(
 )
 
 
+
+
 @app.callback(Output('output', 'children'), Input('target-render', 'data'), prevent_initial_call=True)
-def html_components(data):
+def page_render_html(data):
+    """  Generate static HTML page or Dash Layout
+
+
+    """
     if data.endswith('.py'):
         page = data.split('/')[-1][:-len('.py')]
         return pages[page].layout
     return html.Iframe(src=data, width='100%', height='100%')
         
 
+
+
+
 ###################################################################
-def sidebar_v1(sidebar):
+def sidebar_v1(sidebar:dict):
     """ Compose Sidebar v1 layout component.
     Docs::
 
@@ -217,9 +234,16 @@ def sidebar_v1(sidebar):
     return sidebar_content
 
 
-def render_page(content_layout, homepage):
-    """Main Render Page
+
+
+
+##########################################################################################
+def page_render_main(content_layout, homepage):
+    """  Will generate the Whole page of the App : Side Bar + main
     Docs::
+
+       content_layout :  dir to json file
+
 
         Raises:
             ValueError
@@ -227,39 +251,41 @@ def render_page(content_layout, homepage):
                 - Raised if version is not found in sidebar_content section.
     
     """
+
+    #### Side Generator
     SIDEBAR_VER = {1: sidebar_v1} # Scalable sidebar
+    try :    
+       version         = content_layout['sidebar_content']['version'] 
+       sidebar_content = SIDEBAR_VER[version](content_layout['sidebar_content'])
+    except Exception as e : 
+        print('Side Bar issue', e )
+        raise ValueError("  content_layout['sidebar_content']['version']   ") 
 
-    if 'sidebar_content' not in content_layout.keys():
-        raise ValueError('sidebar_content key not found in layout json')
-    
-    if 'version' not in content_layout['sidebar_content'].keys():
-        raise ValueError('version key not found in sidebar_content section')
-        
-    version = content_layout['sidebar_content']['version'] 
 
-    sidebar_content = SIDEBAR_VER[version](content_layout['sidebar_content'])
-    main_content = html.Div(id="output", style=content_layout['main_content'])
+    #### Main content
+    main_content    = html.Div(id="output", style=content_layout['main_content'])
 
-    app.layout = html.Div([
-                            sidebar_content, 
+    #### Whole : Main + Sidebar
+    app.layout = html.Div([ sidebar_content, 
                             main_content,
                             Store(id='homepage', storage_type='session', data=homepage),
                             Store(id='target-render'), 
                             ])
 
 
+
 def main(content_layout="assets/html_layout.json", homepage="", debug=True, dir_log=""):
-    """ Run main app
+    """ Run main Server Dash App
     Docs::
 
         Args:
-            content_layout (dict, optional):
-                The content layout in JSON format. Default to 'assets/html_layout.json'.
-            homepage (str, optional): 
-                Set Homepage Location. Defaults to "None".
-            debug (boolean, optional):
-                Set dash debug options. Default to 'True'
+            content_layout  json       :  Content layout in JSON format. Default to 'assets/html_layout.json'.
+            homepage (str, optional)   :  Set Homepage Location. 
+            debug (boolean, optional)  :  Set dash debug options. 
     
+
+        Sample layout is here in:  assets/html_layout.json
+
         Raises:
             ValueError
                 Raised if content_type is not 'links', 'html', or 'dash'.
@@ -278,9 +304,20 @@ def main(content_layout="assets/html_layout.json", homepage="", debug=True, dir_
 
     homepage = homepage or content_layout['sidebar_content']['data']['key']
    
-    render_page(content_layout, homepage)
+    page_render_main(content_layout, homepage)
 
     app.run_server(debug=debug)
+
+
+
+
+
+
+
+
+
+
+
 
 
 if __name__ == '__main__':
