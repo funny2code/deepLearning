7d14da2cac77025c08cc018a3fab1d44c18f8896
amarps <amarpanjis@gmail.com>
2022-11-16 20:13:34 +0700

    init benchmark key value db

---
+++ utilmy/db/kvs/go.mod
@@ -0,0 +1,13 @@
+module github.com/arita37/myutil/utilmy/db/kvs
+
+go 1.19
+
+require (
+	github.com/No3371/go-skytable v0.0.0-20220908054633-a0ad52aecce9
+	github.com/go-redis/redis/v8 v8.11.5
+)
+
+require (
+	github.com/cespare/xxhash/v2 v2.1.2 // indirect
+	github.com/dgryski/go-rendezvous v0.0.0-20200823014737-9f7001d12a5f // indirect
+)
---
+++ utilmy/db/kvs/kvdb/kvdb.go
@@ -0,0 +1,122 @@
+package kvdb
+
+import (
+	"context"
+	"net"
+	"sync"
+
+	"github.com/No3371/go-skytable"
+	"github.com/go-redis/redis/v8"
+)
+
+func RunSetXClientsYTimes(x int, y int, port string) {
+	var wg sync.WaitGroup
+
+	for i := 0; i < x; i++ {
+		wg.Add(1)
+		go func() {
+			defer wg.Done()
+			client := redis.NewClient(&redis.Options{
+				Addr:     "localhost:" + port,
+				Password: "", // no password set
+				DB:       0,  // use default DB
+			})
+
+			ctx := context.Background()
+			for j := 0; j < y; j++ {
+				err := client.Set(ctx, "foo", "bar", 0).Err()
+				if err != nil {
+					panic(err)
+				}
+			}
+		}()
+	}
+	wg.Wait()
+}
+
+func RunGetXClientsYTimes(x int, y int, port string) {
+	var wg sync.WaitGroup
+
+	for i := 0; i < x; i++ {
+		wg.Add(1)
+		go func() {
+			defer wg.Done()
+			client := redis.NewClient(&redis.Options{
+				Addr:     "localhost:" + port,
+				Password: "", // no password set
+				DB:       0,  // use default DB
+			})
+
+			ctx := context.Background()
+			err := client.Set(ctx, "foo", "bar", 0).Err()
+			if err != nil {
+				panic(err)
+			}
+
+			for j := 0; j < y; j++ {
+				res := client.Get(ctx, "foo")
+				if res.Val() != "bar" {
+					panic(res)
+				}
+			}
+		}()
+	}
+	wg.Wait()
+}
+
+func SkyTableRunSetXClientsYTimes(x int, y int) {
+	var wg sync.WaitGroup
+
+	for i := 0; i < x; i++ {
+		wg.Add(1)
+		go func() {
+			defer wg.Done()
+			localAddr := &net.TCPAddr{IP: []byte{172, 17, 0, 1}, Port: 6382}
+
+			client, err := skytable.NewConn(localAddr)
+			if err != nil {
+				panic(err)
+			}
+
+			ctx := context.Background()
+			for j := 0; j < y; j++ {
+				err := client.Set(ctx, "foo", "bar")
+				if err != nil {
+					panic(err)
+				}
+			}
+		}()
+	}
+	wg.Wait()
+}
+
+func SkyTableRunGetXClientsYTimes(x int, y int) {
+	var wg sync.WaitGroup
+
+	for i := 0; i < x; i++ {
+		wg.Add(1)
+		go func() {
+			defer wg.Done()
+			localAddr := &net.TCPAddr{IP: []byte{172, 17, 0, 1}, Port: 6382}
+
+			client, err := skytable.NewConn(localAddr)
+			if err != nil {
+				panic(err)
+			}
+
+			ctx := context.Background()
+			err = client.Set(ctx, "foo", "bar")
+			if err != nil {
+				panic(err)
+			}
+
+			for j := 0; j < y; j++ {
+				res, _ := client.Get(ctx, "foo")
+				if res.Value != "bar" {
+					panic(res)
+				}
+			}
+		}()
+	}
+	wg.Wait()
+}
