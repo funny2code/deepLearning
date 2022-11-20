7cd9d02a74fa96cd77145b4f05e9fa8594a4237b
arita37 <noelkev0@gmail.com>
2022-11-20 11:39:13 +0900

    ok

--- utilmy/viz/ddash/app1/pages/form_calc.py
+++ utilmy/viz/ddash/app1/pages/form_calc.py
@@ -11,21 +11,23 @@ from dash import Dash, html
 
 ##########################################################################################
 ### Modular Dash Html Part
+title   = html.H1("AB Test:  N-Sample calculator     ")
 
-title   = html.H1("   AB Test Sample     ")
+ctr     = dbc.Row([ dbc.Label("Baseline  CTR level :",     width = 3),    
+                    dbc.Col( dbc.Input(id= "ctr", type = "number", placeholder = "CTR in %"),            width= 4)],      className   = "mb-3")
 
-ctr     = dbc.Row([ dbc.Label("Baseline  CTR level :",         html_for = "ctr",            width = 3),    dbc.Col( dbc.Input(id= "ctr",           type = "number", placeholder = " CTR in %"), 
-                    width       = 4)],      className   = "mb-3")
-mde     = dbc.Row([ dbc.Label("Minimal Detection Effect :",    html_for = "min_effect",     width = 3),    dbc.Col( dbc.Input(id= "min_effect",    type = "number", placeholder = " MDE in %"), 
-                    width       = 4)],      className   = "mb-3")
-traffic = dbc.Row([ dbc.Label("Daily Traffic :",               html_for = "daily_traffic",  width = 3),    dbc.Col( dbc.Input(id= "daily_traffic", type = "number", placeholder = " 15000"), 
-                    width       = 4)],      className   = "mb-3")
-nvariant= dbc.Row([ dbc.Label("N Variants :",           html_for = "n_variant",  width = 3),    dbc.Col( dbc.Input(id= "n_variant", type = "number", placeholder = "2"), 
-                    width       = 4)],      className   = "mb-3")
+mde     = dbc.Row([ dbc.Label("Minimal Detection Effect :", width = 3),    
+                    dbc.Col( dbc.Input(id= "min_effect",    type = "number", placeholder = "MDE in %"),  width= 4)],      className   = "mb-3")
+
+traffic = dbc.Row([ dbc.Label("Daily Traffic :",  width = 3),    
+                    dbc.Col( dbc.Input(id= "daily_traffic", type = "number", placeholder = "15000"),     width= 4)],      className   = "mb-3")
+
+nvariant= dbc.Row([ dbc.Label("N Variants :", width = 3),    
+                    dbc.Col( dbc.Input(id= "n_variant", type = "number", placeholder = "2"),             width= 4)],      className   = "mb-3")
 
 
-calc    = dbc.Row([ dbc.Label("",                              html_for = "calc",           width = 3),    dbc.Col(dbc.Button(id = "calc",         color = "primary", children  = "Calc" ), 
-                    width = 4)],            className   = "mb-3")
+calc    = dbc.Row([ dbc.Label("",width = 3),    
+                    dbc.Col(dbc.Button(id = "calc",         color = "primary", children  = "Calc" ),  width = 4)],        className   = "mb-3")
 
 result  = dbc.Row([ html.H5("Nsample required ( 95% Confidence, 80% Power) :", ),  html.P(id = "result"),   
                     html.H5("Ndays required:", ),   html.P(id = "result2")])
@@ -40,18 +42,16 @@ layout = dbc.Form([title, ctr, mde, traffic, nvariant, calc, result], style={'pa
 #### Sample BMI (Body Mass Index) calculation
 @callback( Output("result",           "children"),
 [Input("ctr",   "value"),       Input("min_effect",  "value"),  Input("n_variant",   "value"),    Input("calc",   "n_clicks")],  prevent_initial_callback=  True)
-def calculate(ctr, min_effect, n_variant, _):    
+def calc_nsample(ctr, min_effect, n_variant, _):    
     if ctx.triggered_id == 'calc':
         if (ctr is None or min_effect is None or n_variant is None):  return '' 
 
         ctr        = 0.01*float(ctr)
-        min_effect = 0.01*float(min_effect)    ###  relative minimum effect.
+        min_effect = 0.01*max(0.01, float(min_effect))    ###  relative minimum effect.
         n_variant  = float(n_variant) 
 
 
-        ### Per variant, need to doule for mutiple Variants
-        variance   = ctr*(1-ctr)  ### Binonmal
-        nsample  = int( 16 * variance / ( min_effect **2 )   * n_variant )
+        nsample = ab_get_sample(ctr, min_effect, n_variant)
         res  = str(nsample)
         return res
 
@@ -64,19 +64,22 @@ def calc_ndays(ctr, min_effect, n_variant, traffic,  _):
         if (ctr is None or min_effect is None or traffic is None or n_variant is None):  return '' 
 
         ctr        = 0.01*float(ctr)
-        min_effect = 0.01*float(min_effect)  ###  relative minimum effect.
+        min_effect = 0.01*max(0.01, float(min_effect))  ###  relative minimum effect.
         n_variant  = float(n_variant) 
         traffic    = float(traffic)
 
-        variance   = ctr*(1-ctr)             ### Binonmal
 
-        ### Per variant, need to doule for mutiple Variants
-        nsample = int( 16 * variance / ( min_effect **2 )   * n_variant )
+        nsample = ab_get_sample(ctr, min_effect, n_variant)
 
-        ndays =  max(1, int(nsample / traffic) )
+        ndays = max(1, int(nsample / traffic) )
         res   = str(ndays)
         return res
 
 
 
+def ab_get_sample(ctr, min_effect, n_variant):
 
+        ### Per variant, need to doule for mutiple Variants
+        variance = ctr*(1-ctr)  ### Binonmal
+        nsample  = int( 16 * variance / ( min_effect **2 )   * n_variant )
+        return nsample
