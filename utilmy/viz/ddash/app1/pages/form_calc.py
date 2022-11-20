751236a21931ac5a575494ba0ab078848efcf776
Riyad Febrian <riyadfebrian@gmail.com>
2022-11-20 08:36:03 +0700

    fix form layout

--- utilmy/viz/ddash/app1/pages/form_calc.py
+++ utilmy/viz/ddash/app1/pages/form_calc.py
@@ -1,48 +1,32 @@
-import dash_bootstrap_components as dbcmin_effetctmin_effect
+import dash_bootstrap_components as dbc
 from dash.dependencies import Input, Output
 from dash import html, callback, ctx
 
 
 ##########################################################################################
-### https://stackoverflow.com/questions/60299299/grid-dashboard-with-plotly-dash
+### Modular Dash Html Part
 
-layout = html.Div([
-       
-        html.H1("   AB Test Sample     "),
- 
-        dbc.Row([   html.label("   "), dbc.Input(id= "ctr",           type = "number", placeholder = "Baseline  CTR level n %",] )
+title   = html.H1("   AB Test Sample     ")
 
-        dbc.Row([   html.label("   "), dbc.Input(id= "min_effect",    type = "number", placeholder = "Minimal Detection Effect in %")  ])
-
-        dbc.Row([   html.label("   "), dbc.Input(id= "Daily traffic", type = "number", placeholder = " 15000")   ])
-
-
-        
-        html.H5("   ")
-        dbc.Col([ dbc.Button(id = "calc", color = "primary", children = "Calc" )], width = 4)       
-        html.H5("   ")
+ctr     = dbc.Row([ dbc.Label("Baseline  CTR level :",         html_for = "ctr",            width = 3),    dbc.Col( dbc.Input(id= "ctr",           type = "number", placeholder = " CTR in %"), 
+                    width       = 4)],      className   = "mb-3")
+mde     = dbc.Row([ dbc.Label("Minimal Detection Effect :",    html_for = "min_effect",     width = 3),    dbc.Col( dbc.Input(id= "min_effect",    type = "number", placeholder = " MDE in %"), 
+                    width       = 4)],      className   = "mb-3")
+traffic = dbc.Row([ dbc.Label("Daily Traffic :",               html_for = "daily_traffic",  width = 3),    dbc.Col( dbc.Input(id= "daily_traffic", type = "number", placeholder = " 15000"), 
+                    width       = 4)],      className   = "mb-3")
+calc    = dbc.Row([ dbc.Label("",                              html_for = "calc",           width = 3),    dbc.Col(dbc.Button(id = "calc",         color = "primary", children  = "Calc" ), 
+                    width = 4)],            className   = "mb-3")
+result  = dbc.Row([ html.H5("Nsample required ( 95% Confidence, 80% Power) :", ),  html.P(id = "result"),   html.H5("Ndays required:", ),   html.P(id = "result2")])
 
 
-        html.H5("Nsample required ( 95% Confidence, 80% Power) :", ),  html.P(id = "result")
-        html.H5("Ndays required:", ),             html.P(id = "result2")
-
-            ],  width           = 4,
-        ),
-
-            
-        , width = 4),  
-
-
-    ],    
-    className       = "g-3",    style      = {"margin"     : "20px"}
-)
-
+### Constructing Layout
+layout = dbc.Form([title, ctr, mde, traffic, calc, result], style={'padding'  : '20px'})
 
 
 ##########################################################################################
 ################################# Callbacks ##############################################
 #### Sample BMI (Body Mass Index) calculation
-@callback( Output("result",               "children"),
+@callback( Output("result",           "children"),
 [Input("ctr",   "value"),       Input("min_effect",  "value"),   Input("calc",   "n_clicks")],  prevent_initial_callback=  True)
 def calculate(ctr, min_effect, _):    
     if ctx.triggered_id == 'calc':
@@ -59,11 +43,11 @@ def calculate(ctr, min_effect, _):
 
 
 @callback( Output("result2", "children"),
-[Input("ctr", "value"), Input("min_effect", "value"),   Input("traffic", "value"),  Input("calc",  "n_clicks")], prevent_initial_callback=     True)
+[Input("ctr", "value"), Input("min_effect", "value"),   Input("daily_traffic", "value"),  Input("calc",  "n_clicks")], prevent_initial_callback=     True)
 def calc_ndays(ctr, min_effect, traffic,  _):    
     if ctx.triggered_id == 'calc':
         # Parsing from string input to float
-        if (ctr is None or min_effect is None):  return '' 
+        if (ctr is None or min_effect is None or traffic is None):  return '' 
 
         ctr        = float(ctr)
         min_effect = float(min_effect)
@@ -74,6 +58,5 @@ def calc_ndays(ctr, min_effect, traffic,  _):
  
         res  = str(ndays)
         return res
-
 
 
