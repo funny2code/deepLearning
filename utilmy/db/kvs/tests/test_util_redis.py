28eee58debe4297c1e0d07ab2025cfa6a6aeba61
amarps <amarpanjis@gmail.com>
2022-11-24 15:17:17 +0700

    update redis client code

---
+++ utilmy/db/kvs/tests/test_util_redis.py
@@ -0,0 +1,38 @@
+from util_redis import redisClient, ConnectionFailed
+import random, string
+
+def test_connection():
+    # test connection failed
+    try:
+        client = redisClient(host='localsss', port=1123)
+        assert False
+    except ConnectionFailed:
+        assert True
+
+    # test connection success
+    try:
+        client = redisClient(host='localhost', port=6379, db=0)
+        assert True
+    except ConnectionFailed:
+        assert False
+
+def test_getput():
+    client = redisClient(host='localhost', port=6379, db=0)
+    client.put('foo', 'bar')
+    res = client.get('foo').decode('utf8')
+    assert res == 'bar'
+
+def randomStringGenerator(size, chars=string.ascii_lowercase + string.digits):
+    return ''.join(random.choice(chars) for _ in range(size))
+
+def test_getputmulti():
+    client = redisClient(host='localhost', port=6379, db=0)
+    keyvalues = [['a', '1'], ['b', '2'], ['c', '3']]
+    keys = ['a', 'b', 'c']
+
+    client.put_multi(keyvalues, 3)
+    res = client.get_multi(keys, 3)
+    print()
+    for i in range(len(keys)):
+        print(f'index {i}: key {keys[i]}; value {res[i]}')
+        
--- utilmy/db/kvs/util_redis.py
+++ utilmy/db/kvs/util_redis.py
@@ -4,71 +4,77 @@
 
 """
 
+import redis
+
 
 class redisClient:
-	def __init__(self, host = 'localhost', port = 6333, config_file=None, config_keyname= 'redis', config_dict=None):
-      if isinstance(config_dict, dict) :
-         self.cfg = config_dict
+    def __init__(self, host: str = 'localhost', port: int = 6333, config_file: str=None, db=0, config_keyname= 'redis', config_dict=None):
+        if isinstance(config_dict, dict) :
+            self.cfg = config_dict
 
-      elif isinstance(config_file, str):    
-         from utilmy  import config_load
-         self.cfg = config_load(config_file)
-         self.cfg = self.cfg[config_keyname]
+        elif isinstance(config_file, str):    
+            from utilmy  import config_load
+            self.cfg = config_load(config_file)
+            self.cfg = self.cfg[config_keyname]
 
-      PORT = self.cfg['port']
-      HOST = self.cfg['host']
-      transaction  = cfg['transaction']
+        else:
+            self.cfg = {
+                'host': host,
+                'port': port,
+                'db': db
+            }
 
-      self.client = redis.Redis(host='localhost', port=6379, db=0)
-      self.pipe =   self.client.pipeline(transaction=transaction)
+        self.host = self.cfg['host']
+        self.port = self.cfg['port']
+        self.db = self.cfg['db']
 
+        self.client = redis.Redis(host=self.host, port=self.port, db=self.db)
+        try:
+            self.client.ping()
+        except redis.exceptions.ConnectionError: 
+            raise ConnectionFailed("Failed to connect to redis")
 
- 	def connect(self, table):
-		self.client = redisClient(host=self.HOST, port=self.PORT)
+    def get(self, key):
+        return self.client.get(key)
 
+    def put(self, key, val):
+        self.client.set(key, val)
+        return True
 
-   def get(key):
-      return val
+    def put_multi(self, key_values, batch_size=500, transaction=False, nretry=3):
+        n       = len(key_values)
+        n_batch = n // batch_size + 1
+        self.pipe =   self.client.pipeline(transaction=transaction)
 
-   def put(key, val):     
-       return True
+        for k in range(n_batch):
+            i = 0
+            while i < batch_size and k*batch_size+i < n:   
+                self.pipe.hset(i, key_values[k*batch_size+ i][0], key_values[k*batch_size + i][1] )
+                i += 1
 
-   def put_multi(self, key_values, batch_size=500, transaction=False, nretry=3):
+            flag = 0 
+            ii   = 0
+            while flag and ii < nretry:
+                ii =  ii + 1 
+                try :      
+                    self.pipe.execute()
+                    flag = False
+                    ntotal = batch_size
+                except Exception as e: 
+                    return ntotal             
 
-      n       = len(key_values)
-      n_batch = n // batch_size + 1
+    def get_multi(self, keys, batch_size=500, transaction=False):
+        pipe = self.client.pipeline(transaction=transaction)
 
-      for k in range(n_batch):
-         i = 0
-         while i < batch_size and k*batch_size+i < n :   
-               self.pipe.hset(i, key_values[k*batch_size+ i][0], key_values[k*batch_size + i][1] )
-               i += 1
+        n_batch = len(keys) // batch_size
+        res = []
+        for k in range(n_batch):
+            for i in range(batch_size):
+                pipe.hget(i, keys[k*batch_size + i])
 
-         flag = 0 
-         ii   = 0
-         while flag and ii < nretry:0
-            ii =  ii + 1 
-            try :      
-               self.pipe.execute()
-               flag = False
-               ntotal = batch_size
-            except Exception as e: 
+            res = res + self.pipe.execute()
 
-
-      return ntotal             
-
-   def get_multi(self, keys, batch_size=500, transaction=False):
-      # pipe = self.client.pipeline(transaction= transaction)
-
-      n_batch = len(keys) // batch_size
-      res = []
-      for k in range(n_batch):
-         for i in range(batch_size):
-               pipe.hget(i, keys[k*batch_size + i])
-
-         res = res + self.pipe.execute()
-
-      return res
+        return res
 
 
 class RedisQueries(object):
@@ -95,101 +101,5 @@ class RedisQueries(object):
                 siid_to_title[siid] = vals[-1]
         return siid_to_title
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-import redis
-import random
-import string
-
-def hiredisPipeleineSetXTimes(client, keys, values):
-    keylen = len(keys)
-    pipe = client.pipeline(transaction=False)
-    for i in range(keylen):
-        pipe.set(keys[random.randint(0, keylen-1)], values[random.randint(0, keylen-1)])
-    pipe.execute()
-
-def hiredisPipeleineGetXTimes(client , keys):
-    pipe = client.pipeline(transaction=False)
-    for i in keys:
-        pipe.get(i)
-    pipe.execute()
-
-def hiredisPipeleineHSetXTimes(client: redis.Redis, keys, values, batch_size=500):
-    pipe = client.pipeline(transaction=False)
-
-    keylen = len(keys)
-    n_batch = keylen // batch_size
-
-    for k in range(n_batch):
-        for i in range(batch_size):
-            pipe.hset(i, keys[random.randint(0, keylen-1)], values[random.randint(0, keylen-1)])
-
-        pipe.execute()
-        
-
-def hiredisPipeleineHGetXTimes(client: redis.Redis ,keys, batch_size=500):
-    pipe = client.pipeline(transaction=False)
-
-    n_batch = len(keys) // batch_size
-
-    for k in range(n_batch):
-        for i in range(batch_size):
-            pipe.hget(i, keys[i])
-
-        pipe.execute()
-
-
-def redis_get_batch(client: redis.Redis ,keys, batch_size=500):
-    pipe = client.pipeline(transaction=False)
-    n_batch = len(keys) // batch_size
-    result = []
-
-    for k in range(n_batch):
-        for i in range(batch_size):
-            pipe.hget(i, keys[i])
-
-        result.append(pipe.execute())
-
-    return result
-
-
-def randomStringGenerator(size, chars=string.ascii_lowercase + string.digits):
-    return ''.join(random.choice(chars) for _ in range(size))
-
-keys = []
-values = []
-
-for i in range(100000):
-    keys.append(randomStringGenerator(15))
-    values.append(randomStringGenerator(1000))
-
-def test_hiredisSet():
-    client = redis.Redis(host='localhost', port=6379, db=0)
-    hiredisPipeleineSetXTimes(client=client, keys=keys, values=values)
-
-def test_hiredisGet():
-    client = redis.Redis(host='localhost', port=6379, db=0)
-    hiredisPipeleineGetXTimes(client=client, keys=keys)
-
-def test_hiredisHSet():
-    client = redis.Redis(host='localhost', port=6379, db=0)
-    hiredisPipeleineHSetXTimes(client=client, keys=keys, values=values)
-
-def test_hiredisHGet():
-    client = redis.Redis(host='localhost', port=6379, db=0)
-    hiredisPipeleineHGetXTimes(client=client, keys=keys)
-
+class ConnectionFailed(Exception):
+    pass
