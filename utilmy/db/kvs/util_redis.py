b40f6f643c941239eea2bb896e4f177dba4c2050
arita37 <noelkev0@gmail.com>
2022-11-26 21:20:13 +0900

    ok

--- utilmy/db/kvs/util_redis.py
+++ utilmy/db/kvs/util_redis.py
@@ -6,9 +6,12 @@ Docs::
 
 
 """
-import redis, time
+import time, random, string
+from dataclasses import dataclass
+import redis
+from redis.cluster import RedisCluster, ClusterNode
+
 from utilmy import log
-import random, string
 
 
 #################################################################################
@@ -17,7 +20,6 @@ def test_all():
    test_connection()
    test_getput()
    test_getputmulti()
-
 
 
 def test_connection():
@@ -60,9 +62,154 @@ def test_getputmulti():
         
 
 
+########## Cluster ##################################################################
+def test_cluster1():
+    # test connection failed
+    try:
+        config = RedisClusterConfig('localssss', 6379, [6379, 6380, 6381, 6382, 6383, 6384], 'bitnami')
+        client = RedisClusterClient(config)
+        assert False
+    except redis.exceptions.RedisClusterException:
+        assert True
+
+def test_cluster2():
+    # test connection success
+    try:
+        config = RedisClusterConfig('localhost', 6379, [6379, 6380, 6381, 6382, 6383, 6384], 'bitnami')
+        client = RedisClusterClient(config)
+        assert True
+    except redis.exceptions.RedisClusterException:
+        assert False
+
+
+def test_cluster3():
+    config = RedisClusterConfig('localhost', 6379, [6379, 6380, 6381, 6382, 6383, 6384], 'bitnami')
+    client = RedisClusterClient(config)
+    client.put('foo', 'bar')
+    res = client.get('foo').decode('utf8')
+    assert res == 'bar'
+
+
+# def test_getputmulti():
+#     config = RedisClusterConfig('localhost', 6379, [6379, 6380, 6381, 6382, 6383, 6384], 'bitnami')
+#     client = RedisClusterClient(config)
+#     keyvalues = [['a', '1'], ['b', '2'], ['c', '3']]
+#     keys = ['a', 'b', 'c']
+
+#     client.put_multi(keyvalues, 1)
+#     res = client.get_multi(keys, 1)
+#     print()
+#     for i in range(len(keys)):
+#         print(f'index {i}: key {keys[i]}; value {res[i]}')
+
 
 #################################################################################
 #################################################################################
+@dataclass
+class RedisClusterConfig:
+    host: str
+    port: str | int
+    node_ports: list()
+    password: str
+
+
+class RedisClusterClient:
+    def __init__(self, clusterConfig : RedisClusterConfig, read_from_replicas=True, encoding='utf-8', decode_response=True):
+        nodes = list()
+        for port in clusterConfig.node_ports:
+            nodes.append(ClusterNode(host=clusterConfig.host, port=port))
+
+        self.client = RedisCluster(
+            startup_nodes=nodes,
+            read_from_replicas=read_from_replicas,
+            host=clusterConfig.host,
+            password=clusterConfig.password,
+            port=clusterConfig.port)
+
+        
+        self.client.ping()
+
+
+    def get(self, key):
+        """get value from redis using key
+        """
+        return self.client.get(key)
+
+
+    def put(self, key, val):
+        """set value to key
+        """
+        self.client.set(key, val)
+        return True
+
+    def put_multi(self, key_values, batch_size=500, transaction=False, nretry=3):
+        """set multiple keys and values to redis
+        
+        Parameters:
+            key_values ([[key, value], ]): key and value as 2D list
+            batch_size (int): number of batch.
+            transaction (bool): enable MULTI and EXEC statements.
+            nretry (int): number of retry
+        """
+        n         =  len(key_values)
+        n_batch   =  n // batch_size + 1
+        self.pipe =  self.client.pipeline(transaction=transaction)
+
+        ntotal = 0  
+        for k in range(n_batch):
+            for i in range(batch_size):
+                ix  = k*batch_size + i 
+                if ix >= n: break 
+                key,val = key_values[ix]   
+                self.pipe.hset(i, key, val )
+                i += 1
+
+            flag = True 
+            ii   = 0
+            while flag and ii < nretry:
+                ii =  ii + 1 
+                try :      
+                    self.pipe.execute()
+                    flag = False
+                    ntotal += batch_size
+                except Exception as e: 
+                    log(e)
+                    time.sleep(2)
+                  
+        return ntotal 
+
+
+    def get_multi(self, keys, batch_size=500, transaction=False):
+        """get multiple value using list of keys
+        Parameters:
+            keys (list(string)): list of keys
+            batch_size (int): number of batch.
+            transaction (bool): enable MULTI and EXEC statements.
+        """
+        pipe = self.client.pipeline(transaction=transaction)
+
+        n       = len(keys)
+        n_batch = n // batch_size  + 1
+        res = []
+        for k in range(n_batch):
+            for i in range(batch_size):
+                ix  = k*batch_size + i
+                if ix >= n : break
+                try :
+                   pipe.hget(i, keys[ix])
+                except Exception as e :
+                  log(e)   
+                  time.sleep(5)
+                  pipe.hget(i, keys[ix])
+
+
+            resk =  self.pipe.execute()
+            res  = res + resk
+
+        return 
+
+
+
 class redisClient:
     def __init__(self, host:  str = 'localhost', port: int = 6333, user='', password='',
                  config_file: str=None, db=0, config_keyname= 'redis', config_dict=None):
@@ -217,6 +364,8 @@ class ConnectionFailed(Exception):
 
 class AuthenticationFailed(Exception):
     pass
+
+
 
 
 ############################################################################################################
