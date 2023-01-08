df9ba9a38c627c67fed335e5847b76309c273086
PN <arita37@users.noreply.github.com>
2023-01-09 00:43:27 +0900

    ok

--- dockers/localstack/v2/start_localstack.sh
+++ dockers/localstack/v2/start_localstack.sh
@@ -12,8 +12,14 @@
 
    #### Set permanent endpoints
    #### https://stackoverflow.com/questions/52494196/is-there-any-way-to-specify-endpoint-url-in-aws-cli-config-file
-   
 
+
+   ### function aws() { /usr/local/bin/aws --endpoint foo "${@}" } 
+    # for some reason that didn't work for me, so I used a function: function aws() { /usr/local/bin/aws --endpoint foo "${@}" } then later unset -f aws – 
+   # This command must be written in ~/.bash_aliases or ~/.bashrc – 
+
+   ### function aws() { /usr/local/bin/aws --endpoint foo "${@}" } 
+    
 
    ./check_localstack.sh
 
