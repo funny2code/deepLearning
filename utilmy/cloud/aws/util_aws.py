0e605d102cb86ae7e0fdb427de20c63cb05eebdd
PN <arita37@users.noreply.github.com>
2023-01-17 22:05:44 +0900

    aws

--- utilmy/cloud/aws/util_aws.py
+++ utilmy/cloud/aws/util_aws.py
@@ -164,6 +164,153 @@ def test_topandas():
         
 
 
+
+
+######## Refrestable session
+def aws_get_session(profile_name:str="", session=None):
+    """ Get session
+    Docs::
+     
+        1) From local cache  .aws/cli/cache .aws/cli/boto
+        2) From ENV Variable: AWS_ACCESS_KEY_ID, AWS_SECRET_ACCESS_KEY
+
+        Returns: Boto3 session
+    """
+    from botocore import credentials
+    import botocore.session
+    import glob
+
+    if session is not None :
+        return session
+
+    for fi in [ '.aws/cli/cache', '.aws/boto/cache' ]:
+        # By default the cache path is ~/.aws/boto/cache
+        cli_cache = os.path.join(os.path.expanduser('~'), fi)
+        if len(glob.glob(cli_cache + "/*" ) ) > 0 :
+            # Construct botocore session with cache
+            session = botocore.session.get_session()
+            session.get_component('credential_provider').get_provider('assume-role').cache = credentials.JSONFileCache(cli_cache)
+
+            # Create boto3 client from session
+            client = boto3.Session(botocore_session=session)
+            log2("Using ", fi, client)
+            return client
+ 
+
+    client = boto3.Session( aws_access_key_id    = os.environ['AWS_ACCESS_KEY_ID'],
+                            aws_secret_access_key= os.environ['AWS_SECRET'],)
+    log2("using ENV Variables ", client)
+    return client
+
+
+
+from uuid import uuid4
+from datetime import datetime
+from time import time
+
+import boto3
+from boto3 import Session
+from botocore.credentials import RefreshableCredentials
+from botocore.session import get_session
+
+
+
+class BotoSession:
+    """
+    Boto Helper class which lets us create refreshable session, so that we can cache the client or resource.
+    Usage
+    -----
+    session = BotoSession().refreshable_session()
+    client = session.client("s3") # we now can cache this client object without worrying about expiring credentials
+
+    # check for max session duration
+    # https://docs.aws.amazon.com/IAM/latest/UserGuide/id_roles_use.html#id_roles_use_view-role-max-session
+    """
+    def __init__(
+        self,
+        region_name: str = None,
+        profile_name: str = None,
+        sts_arn: str = None,
+        session_name: str = None,
+    ):
+        """
+        Initialize `BotoSession`
+        Parameters
+        ----------
+        region : str (optional)
+            Default region when creating new connection.
+        profile_name : str (optional)
+            The name of a profile to use.
+        sts_arn : str (optional)
+            The role arn to sts before creating session.
+        session_name : str (optional)
+            An identifier for the assumed role session. (required when `sts_arn` is given)
+        """
+
+        self.region_name = region_name
+        self.profile_name = profile_name
+        self.sts_arn = sts_arn
+
+        # read why RoleSessionName is important https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/sts.html
+        self.session_name = session_name or uuid4().hex
+
+    def __get_session_credentials(self, ttl=900):
+        """
+        Get session credentials
+        """
+        credentials = {}
+        session = Session(region_name=self.region_name, profile_name=self.profile_name)
+
+        # if sts_arn is given, get credential by assuming given role
+        if self.sts_arn:
+            sts_client = session.client(self.service_name, region_name=self.region_name)
+            response = sts_client.assume_role(
+                RoleArn=self.sts_arn,
+                RoleSessionName=self.session_name,
+                DurationSeconds=ttl,
+            ).get("Credentials")
+
+            credentials = {
+                "access_key": response.get("AccessKeyId"),
+                "secret_key": response.get("SecretAccessKey"),
+                "token": response.get("SessionToken"),
+                "expiry_time": response.get("Expiration").isoformat(),
+            }
+        else:
+            session_credentials = session.get_credentials().__dict__
+            credentials = {
+                "access_key": session_credentials.get("access_key"),
+                "secret_key": session_credentials.get("secret_key"),
+                "token": session_credentials.get("token"),
+                "expiry_time": datetime.fromtimestamp(time() + TTL).isoformat(),
+            }
+
+        return credentials
+
+    def refreshable_session(self) -> Session:
+        """
+        Get refreshable boto3 session.
+        """
+        try:
+            # get refreshable credentials
+            refreshable_credentials = RefreshableCredentials.create_from_metadata(
+                metadata=self.__get_session_credentials(),
+                refresh_using=self.__get_session_credentials,
+                method="sts-assume-role",
+            )
+
+            # attach refreshable credentials current session
+            session = get_session()
+            session._credentials = refreshable_credentials
+            session.set_config_variable("region", self.region_name)
+            autorefresh_session = Session(botocore_session=session)
+
+            return autorefresh_session
+
+        except:
+            return boto3.Session()
+
+
 ##### List of JSON ######################################################################################
 def s3_get_filelist(path_s3="/mybucket1/mybucket2/", suffix=".json"):
     """     # Get all json files in a S3 bucket
