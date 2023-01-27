0ea05bf886e3a3b654701b896fd5f007eba05db9
PN <arita37@users.noreply.github.com>
2023-01-27 18:28:17 +0900

    details

--- utilmy/abash.py
+++ utilmy/abash.py
@@ -1,6 +1,28 @@
 """ Shorcuts for Bash
     Docs::
 
+    login shell: A login shell logs you into the system as a specific user. Necessary for this is a username and password. When you hit ctrl+alt+F1 to login into a virtual terminal you get after successful login: a login shell (that is interactive). Sourced files:
+
+    /etc/profile and ~/.profile for Bourne compatible shells (and /etc/profile.d/*)
+    ~/.bash_profile for bash
+    /etc/zprofile and ~/.zprofile for zsh
+    /etc/csh.login and ~/.login for csh
+
+
+    non-login shell: A shell that is executed without logging in. Necessary for this is a current logged in user. When you open a graphic terminal in gnome, it is a non-login (interactive) shell. Sourced files:
+
+    /etc/bashrc and ~/.bashrc for bash
+    interactive shell: A shell (login or non-login) where you can interactively type or interrupt commands, for example, a gnome terminal (non-login) or a virtual terminal (login). In an interactive shell the prompt variable must be set ($PS1). Sourced files:
+    /etc/profile and ~/.profile
+
+    /etc/bashrc or /etc/bash.bashrc for bash
+    non-interactive shell: A (sub)shell that is probably run from an automated process. You will see neither input nor output when the calling process doesn't handle it. That shell is normally a non-login shell, because the calling user has logged in already. A shell running a script is always a non-interactive shell, but the script can emulate an interactive shell by prompting the user to input values. Sourced files:
+    /etc/bashrc or /etc/bash.bashrc for bash (but, mostly you see this at the beginning of the script: [ -z "$PS1" ] && return. That means don't do anything if it's a non-interactive shell).
+
+    depending on shell; some of them read the file in the $ENV variable.
+
+
+
         ps faux  : Show all processe with sub-trees --> good to deliete extra bash in docker.
 
         df -h   :  Disk space size
